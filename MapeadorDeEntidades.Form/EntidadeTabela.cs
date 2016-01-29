7560869975b429f727ca9d934b84e7358ca9b3a0
Antonio <antonio@engsolutions.com.br>
2016-01-29 11:49:18 -0200

    Iniciando repositorio.

---
+++ MapeadorDeEntidades.Form/EntidadeTabela.cs
@@ -0,0 +1,16 @@
+﻿namespace MapeadorDeEntidades.Form
+{
+    public class EntidadeTabela
+    {
+        public string COLUMN_NAME { get; set; }
+
+        public string DATA_TYPE { get; set; }
+
+        public decimal CHAR_LENGTH { get; set; }
+
+        public string NULLABLE { get; set; }
+
+        public string COMMENTS { get; set; }
+
+    }
+}
