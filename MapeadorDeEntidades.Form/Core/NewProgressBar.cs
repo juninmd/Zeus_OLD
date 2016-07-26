2dc4d9305bbc0121651396310e9196bdea1506d6
Antonio <antonio@engsolutions.com.br>
2016-07-26 16:35:37 -0300

    nova barrinha de status

---
+++ MapeadorDeEntidades.Form/Core/NewProgressBar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapeadorDeEntidades.Form.Core
+{
+    public class NewProgressBar : ProgressBar
+    {
+        private ProgressBar progressBar1;
+
+        public NewProgressBar()
+        {
+            this.SetStyle(ControlStyles.UserPaint, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle rec = e.ClipRectangle;
+
+            rec.Width = (int)(rec.Width * ((double)Value / Maximum)) - 4;
+            if (ProgressBarRenderer.IsSupported)
+                ProgressBarRenderer.DrawHorizontalBar(e.Graphics, e.ClipRectangle);
+            rec.Height = rec.Height - 4;
+            e.Graphics.FillRectangle(Brushes.BlueViolet, 2, 2, rec.Width, rec.Height);
+        }
+
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 0;
+            this.ResumeLayout(false);
+
+        }
+    }
+}
