f3baa5b566125ca576d1b2d7251d8ad1434f003e
Antonio <antonio@engsolutions.com.br>
2016-07-26 17:15:33 -0300

    Adiciionado ICONE

--- MapeadorDeEntidades.Form/formPrincipal.Designer.cs
+++ MapeadorDeEntidades.Form/formPrincipal.Designer.cs
@@ -352,6 +352,7 @@ namespace MapeadorDeEntidades.Form
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "formPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
