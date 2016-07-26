8db7111563890af0144d58d6bc9b4f9d697719dc
Antonio <antonio@engsolutions.com.br>
2016-07-26 17:15:33 -0300

    Adiciionado ICONE

--- MapeadorDeEntidades.Form/formConnectionString.Designer.cs
+++ MapeadorDeEntidades.Form/formConnectionString.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formConnectionString));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
@@ -107,6 +108,7 @@
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "formConnectionString";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
