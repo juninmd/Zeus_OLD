819afb6e1e2e9ede3fe2e6da3941759aadb1881c
Antonio <antonio@engsolutions.com.br>
2016-07-27 14:24:20 -0300

    Mapeamento SQL - Entidade 90%

--- MapeadorDeEntidades.Form/Core/SGBD/Microsoft SQL/SQLTables.cs
+++ MapeadorDeEntidades.Form/Core/SGBD/Microsoft SQL/SQLTables.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using MapeadorDeEntidades.Form.Core.SGBD.Oracle;
 
 namespace MapeadorDeEntidades.Form.Core.SGBD.Microsoft_SQL
 {
@@ -21,7 +22,45 @@ namespace MapeadorDeEntidades.Form.Core.SGBD.Microsoft_SQL
 
         public List<SQLEntidadeTabela> ListarAtributos(string nomeTabela)
         {
+            BeginNewStatement("use apolo;" +
+            "SELECT OBJECT_SCHEMA_NAME(T.[object_id]," +
+            " DB_ID()) AS[Schema], " +
+            "AC.[name] AS[column_name], " +
+            "TY.[name] AS DATA_TYPE, " +
+            "AC.[max_length] AS CHAR_LENGTH, " +
+            "AC.[precision] AS DATA_PRECISION, " +
+            "AC.[scale] AS DATA_SCALE, " +
+            "AC.[is_nullable] AS NULLABLE " +
+            "FROM sys.[tables] AS T " +
+            "INNER JOIN sys.[all_columns] AC ON " +
+            "T.[object_id] = AC.[object_id] " +
+            "INNER JOIN sys.[types] TY ON " +
+            "AC.[system_type_id] = TY.[system_type_id] AND " +
+            "AC.[user_type_id] = TY.[user_type_id] " +
+            "WHERE T.[is_ms_shipped] = 0 " +
+            "AND T.[is_ms_shipped] = 0 " +
+            $"AND T.[name] = '{nomeTabela}' " +
+            "ORDER BY T.[name], AC.[column_id]");
+
+            OpenConnection();
+
             var lista = new List<SQLEntidadeTabela>();
+
+            using (var r = ExecuteReader())
+                while (r.Read())
+                {
+                    lista.Add(new SQLEntidadeTabela
+                    {
+                        COLUMN_NAME = r.GetValueOrDefault<string>("COLUMN_NAME"),
+                        DATA_TYPE = r.GetValueOrDefault<string>("DATA_TYPE"),
+                        CHAR_LENGTH = r.GetValueOrDefault<short>("CHAR_LENGTH"),
+                        DATA_PRECISION = r.GetValueOrDefault<byte?>("DATA_PRECISION"),
+                        DATA_SCALE = r.GetValueOrDefault<byte?>("DATA_SCALE"),
+                        NULLABLE = r.GetValueOrDefault<bool>("NULLABLE"),
+                        COMMENTS = "",
+                    });
+                };
+
             return lista;
         }
     }
--- MapeadorDeEntidades.Form/Linguagens/CSharp/SQL/CSharpSQLEntidade.cs
+++ MapeadorDeEntidades.Form/Linguagens/CSharp/SQL/CSharpSQLEntidade.cs
@@ -6,14 +6,16 @@ namespace MapeadorDeEntidades.Form.Linguagens.CSharp.SQL
 {
     public class CSharpSQLEntity : BaseEntity
     {
-        private string GetTypeAtribute(string tipoAttr, string aceitaNull)
+        private string GetTypeAtribute(string tipoAttr, bool aceitaNull)
         {
             switch (tipoAttr)
             {
-                case "DATE":
-                    return "DateTime" + (IsNullabe(aceitaNull) ? "?" : "");
+                case "int":
+                    return "int" + ((aceitaNull) ? "?" : "");
+                case "datetime":
+                    return "DateTime" + ((aceitaNull) ? "?" : "");
                 case "NUMBER":
-                    return "long" + (IsNullabe(aceitaNull) ? "?" : "");
+                    return "long" + ((aceitaNull) ? "?" : "");
                 default:
                     return "string";
             }
