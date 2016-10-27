71215d929809b4a4f485e9e27dfe47d9754048e1
Antonio <antonio@engsolutions.com.br>
2016-10-27 09:01:13 -0200

    Criação de Sequence

---
+++ MapeadorDeEntidades.Form/Core/SGBD/Oracle/Sequence/OracleOrquestradorSequence.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using MapeadorDeEntidades.Form.Utilidade;
+
+namespace MapeadorDeEntidades.Form.Core.SGBD.Oracle.Sequence
+{
+    public class OracleOrquestradorSequence
+    {
+        public RequestMessage<string> Oracle(FolderBrowserDialog salvar)
+        {
+            try
+            {
+                int max = ParamtersInput.NomeTabelas.Count;
+                var i = 0;
+                var local = salvar.SelectedPath + "\\";
+
+                foreach (var nomeTabela in ParamtersInput.NomeTabelas)
+                {
+                    i++;
+                    Util.Barra((int)((((decimal)i / max) * 100)));
+                    Util.Status($"Processando tabela: {nomeTabela}");
+
+                    var instancia = new OracleSequence().Init(nomeTabela);
+                    File.WriteAllText(local + $"{nomeTabela}_SEQUENCE.sql", instancia);
+                }
+
+                return new RequestMessage<string>()
+                {
+                    Message = "Processamento concluído com sucesso!",
+                    StatusCode = System.Net.HttpStatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new RequestMessage<string>()
+                {
+                    Message = "Falha no sistema!",
+                    TechnicalMessage = ex.Message,
+                    StatusCode = System.Net.HttpStatusCode.InternalServerError
+                };
+            }
+        }
+    }
+}
