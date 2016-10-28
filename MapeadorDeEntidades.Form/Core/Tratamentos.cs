1c91d581d3a96901130c6a4b4be7ed85a4b2a774
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2016-10-27 22:34:57 -0200

    Melhorias não comitadas

--- MapeadorDeEntidades.Form/Core/Tratamentos.cs
+++ MapeadorDeEntidades.Form/Core/Tratamentos.cs
@@ -7,6 +7,7 @@
             return nome.Replace("MAG_T", "").Replace("_PDL_", "").Replace("_CA_", "");
         }
         public static string TratarNomePackage(this string nome) { return nome.Replace("_T_", "_PG_"); }
+        public static string TratarNomeBase(this string nome) { return $"\"{nome.Split(';')[1].Split('=')[1]}\""; }
 
         public static string TratarNomeSequence(this string nome)
         {
