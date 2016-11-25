83dcb20e83e8d6174402b819373b30d65527d193
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2016-11-25 00:20:16 -0200

    Melhorias com java + mysql

---
+++ MapeadorDeEntidades.Form/Core/Char.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace MapeadorDeEntidades.Form.Core
+{
+    public static class Char
+    {
+        public static string ToFirstCharToUpper(this string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                return null;
+            return input.First().ToString().ToUpper() + input.Substring(1).ToLower();
+        }
+    }
+}
