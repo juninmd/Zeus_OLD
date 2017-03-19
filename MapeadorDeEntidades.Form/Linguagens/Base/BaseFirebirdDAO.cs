1785e33a7f4df80f659479ca9d7cfc3493518956
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2017-03-19 18:58:00 -0300

    DirectAcess Firebird

---
+++ MapeadorDeEntidades.Form/Linguagens/Base/BaseFirebirdDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Zeus.Core.SGBD.Firebird;
+
+namespace Zeus.Linguagens.Base
+{
+    public class BaseFirebirdDAO
+    {
+        protected static string N => Environment.NewLine;
+        public BaseFirebirdDAO(string nomeTabela)
+        {
+            NomeTabela = nomeTabela;
+        }
+        public string NomeTabela { get; set; }
+        public List<FirebirdEntidadeTabela> ListaAtributosTabela => new FirebirdTables().ListarAtributos(NomeTabela);
+    }
+}
--- MapeadorDeEntidades.Form/Linguagens/CSharp/Firebird/Direct/CSharpFirebirdDirect.cs
+++ MapeadorDeEntidades.Form/Linguagens/CSharp/Firebird/Direct/CSharpFirebirdDirect.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using Zeus.Core;
-using Zeus.Core.SGBD.Firebird;
 using Zeus.Linguagens.Base;
-using Zeus.Linguagens.CSharp.Firebird;
 
-namespace Zeus.Linguagens.Java.MySql.Procedure
+namespace Zeus.Linguagens.CSharp.Firebird.Direct
 {
-    public class CSharpFirebirdDirect : BaseMySqlDAO
+    public class CSharpFirebirdDirect : BaseFirebirdDAO
     {
 
         public CSharpFirebirdDirect(string nomeTabela) : base(nomeTabela)
@@ -19,63 +15,86 @@ namespace Zeus.Linguagens.Java.MySql.Procedure
         private StringBuilder Imports()
         {
             var imports = new StringBuilder();
-            imports.Append($"using System.Windows.Forms;{N}");
+            imports.Append($"using System.Windows.Forms;{N}{N}");
             return imports;
         }
 
         private StringBuilder GetById()
         {
             var get = new StringBuilder();
-            get.Append($"	public {NomeTabela} GetById(int ID) throws Exception{N}");
-            get.Append($"	{{{N}");
-            get.Append($"			ResultSet rs = ExecuteReader(\"SELECT * FROM {NomeTabela} WHERE {ListaAtributosTabela.First()} = \"+ID);{N}");
-            get.Append($"			if(rs.next()){{{N}");
-            get.Append($"				{NomeTabela} resposta = new {NomeTabela}();{N}");
-            foreach (var att in ListaAtributosTabela)
-            {
-                get.Append($"                        {att.COLUMN_NAME} = \"{att.COLUMN_NAME}\".GetValueOrDefault<{CSharpTypesFirebird.GetTypeAtribute(att.DATA_TYPE, false)}>(reader){N}");
-            }
-            get.Append($"				return resposta;{N}");
-            get.Append($"			}}{N}");
-            get.Append($"			return null;{N}");
-            get.Append($"	}}{N}");
+            get.Append($"    	public IDataReader GetById(int ID){N}");
+            get.Append($"    	{{{N}");
+            get.Append($"    	 	var sql = $\"SELECT * FROM {NomeTabela} WHERE {ListaAtributosTabela.First().FIELD_NAME} = {{ ID }}\";{N}");
+            get.Append($"    	 	return ExecuteReader(sql);{N}");
+            get.Append($"    	}}{N}");
             return get;
         }
 
         private StringBuilder GetAll()
         {
             var get = new StringBuilder();
-            get.Append($"	public List<{NomeTabela}> GetAll() throws Exception{N}");
-            get.Append($"	{{{N}");
-            get.Append($"	}}{N}");
+            get.Append($"    	public DataTable GetAll(){N}");
+            get.Append($"    	{{{N}");
+            get.Append($"    	 	var datatable = new DataTable();{N}");
+            get.Append($"    	 	var sql = $\"SELECT * FROM {NomeTabela}\";{N}");
+            get.Append($"    	 	return ExecuteReaderDt(sql);{N}");
+            get.Append($"    	}}{N}");
             return get;
         }
 
         private StringBuilder Add()
         {
             var get = new StringBuilder();
-            get.Append($"	public void Add({NomeTabela} entidade) throws Exception{N}");
-            get.Append($"	{{{N}");
-            get.Append($"	}}{N}");
+            get.Append($"    	public void Insert({NomeTabela}Model entidade){N}");
+            get.Append($"    	{{{N}");
+            get.Append($"    	 	var sql = $\"INSERT INTO {NomeTabela} VALUES\"{N}");
+            get.Append(AddParams());
+            get.Append($"    	 	ExecuteNonResult(sql); {N}");
+            get.Append($"    	 	MessageBox.Show(\"Registro inserido com sucesso !!!\"); {N}");
+            get.Append($"    	}}{N}");
             return get;
+        }
+        private StringBuilder AddParams()
+        {
+            var param = new StringBuilder();
+            var count = ListaAtributosTabela.Count;
+            if (count == 0)
+                return param;
+
+            param.Append($"    		+$\"({{entidade.{ListaAtributosTabela[0].FIELD_NAME}}}\"+{N}");
+            for (int i = 1; i < count - 1; i++)
+            {
+                param.Append($"    		+$\"{{entidade.{ListaAtributosTabela[i].FIELD_NAME}}},\"+{N}");
+            }
+            param.Append($"    		+$\"{{entidade.{ListaAtributosTabela[count - 1].FIELD_NAME}}})\";{N}");
+            return param;
         }
 
         private StringBuilder Update()
         {
             var get = new StringBuilder();
-            get.Append($"	public void Update({NomeTabela} entidade) throws Exception{N}");
-            get.Append($"	{{{N}");
-            get.Append($"	}}{N}");
+            get.Append($"    	public void Update({NomeTabela}Model entidade){N}");
+            get.Append($"    	{{{N}");
+            get.Append($"    	 	var sql = $\"UPDATE {NomeTabela} SET\" + {N}");
+            foreach (var item in ListaAtributosTabela)
+            {
+                get.Append($"    		+\"{item.FIELD_NAME} = \"+{item.FIELD_NAME}" + N);
+            }
+            get.Append($"    	 	+$\"WHERE {ListaAtributosTabela.First().FIELD_NAME} = {{ entidade.{ListaAtributosTabela.First().FIELD_NAME}}};\" {N}");
+            get.Append($"    	 	ExecuteNonResult(sql); {N}");
+            get.Append($"    	 	MessageBox.Show(\"Registro gravado com sucesso !!!\");{N}");
+            get.Append($"    	}}{N}");
             return get;
         }
         private StringBuilder Delete()
         {
-
             var get = new StringBuilder();
-            get.Append($"	public void Delete(int ID) throws Exception{N}");
-            get.Append($"	{{{N}");
-            get.Append($"	}}{N}");
-            get.Append($"}}{N}");
+            get.Append($"    	public void Delete(int ID){N}");
+            get.Append($"    	{{{N}");
+            get.Append($"    	 	var sql = $\"DELETE FROM {NomeTabela} WHERE {ListaAtributosTabela.First().FIELD_NAME} = {{ ID }}\"{N}");
+            get.Append($"    	 	ExecuteNonResult(sql); {N}");
+            get.Append($"    	 	MessageBox.Show(\"Registro excluído com sucesso !!!\"); {N}");
+            get.Append($"    	}}{N}");
             return get;
         }
 
@@ -84,23 +103,19 @@ namespace Zeus.Linguagens.Java.MySql.Procedure
             var classe = new StringBuilder();
             classe.Append(Imports());
             classe.Append($"namespace meuprojeto{N}{{{N}");
-            classe.Append($"public class {NomeTabela}Dao : FirebirdRepository {{ {N}{N}");
+            classe.Append($"    public class {NomeTabela.ToFirstCharToUpper()}Dao : FirebirdRepository {{ {N}{N}");
             classe.Append(GetById());
+            classe.Append(N);
             classe.Append(GetAll());
+            classe.Append(N);
             classe.Append(Add());
+            classe.Append(N);
             classe.Append(Update());
+            classe.Append(N);
             classe.Append(Delete());
+            classe.Append($"    }}{N}");
+            classe.Append($"}}");
             return classe;
-        }
-
-        private StringBuilder GetListaItensGetById(List<FirebirdEntidadeTabela> ListaAtributosTabela)
-        {
-            var atributoText = new StringBuilder();
-            foreach (var item in ListaAtributosTabela)
-            {
-                atributoText.Append($"                        {item.FIELD_NAME} = \"{item.FIELD_NAME}\".GetValueOrDefault<{CSharpTypesFirebird.GetTypeAtribute(item.FIELD_NAME, item.IS_NULLABLE)}>(reader)," + N);
-            }
-            return atributoText;
         }
     }
 }
