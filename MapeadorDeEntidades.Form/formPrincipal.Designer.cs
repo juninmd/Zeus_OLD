974aeba106bcaacc237ab85e2110353aae15b18f
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2017-05-29 21:58:25 -0300

    double buffer

--- MapeadorDeEntidades.Form/formPrincipal.Designer.cs
+++ MapeadorDeEntidades.Form/formPrincipal.Designer.cs
@@ -588,6 +588,7 @@ namespace Zeus
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
