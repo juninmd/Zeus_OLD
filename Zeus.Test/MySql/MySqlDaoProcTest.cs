f907b3808bb3b9654239ae21aac36c4422f0100c
juninmd <jr_acn@yahoo.com.br>
2017-08-15 20:37:05 -0300

    Mysql full

---
+++ Zeus.Test/MySql/MySqlDaoProcTest.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zeus.Core;
+using Zeus.Middleware;
+
+namespace Zeus.Test.MySql
+{
+    [TestClass]
+    public class MySqlDaoProcTest : BaseMysqlTest
+    {
+        public MySqlDaoProcTest()
+        {
+            ParamtersInput.Procedure = true;
+        }
+
+        [TestMethod]
+        public void GenerateCSharp()
+        {
+            ParamtersInput.Linguagem = 1;
+            var chamada = new OrquestradorChamada().Generate();
+            Assert.AreEqual(HttpStatusCode.OK, chamada.StatusCode, chamada.Message);
+            Assert.AreEqual(1, Directory.GetFiles(ParamtersInput.SelectedPath).Length, "Arquivo não foi encontrado.");
+            Directory.Delete(ParamtersInput.SelectedPath, true);
+        }
+
+        [TestMethod]
+        public void GenerateJava()
+        {
+            ParamtersInput.Linguagem = 2;
+            var chamada = new OrquestradorChamada().Generate();
+            Assert.AreEqual(HttpStatusCode.OK, chamada.StatusCode, chamada.Message);
+            Assert.AreEqual(1, Directory.GetFiles(ParamtersInput.SelectedPath).Length, "Arquivo não foi encontrado.");
+            Directory.Delete(ParamtersInput.SelectedPath, true);
+        }
+
+        [TestMethod]
+        public void GenerateNode()
+        {
+            ParamtersInput.Linguagem = 3;
+            var chamada = new OrquestradorChamada().Generate();
+            Assert.AreEqual(HttpStatusCode.OK, chamada.StatusCode, chamada.Message);
+            Assert.AreEqual(1, Directory.GetFiles(ParamtersInput.SelectedPath).Length, "Arquivo não foi encontrado.");
+            Directory.Delete(ParamtersInput.SelectedPath, true);
+        }
+    }
+}
---
+++ Zeus.Test/MySql/MySqlEntityTest.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zeus.Core;
+using Zeus.Middleware;
+
+namespace Zeus.Test.MySql
+{
+    [TestClass]
+    public class MySqlEntityTest : BaseMysqlTest
+    {
+        public MySqlEntityTest()
+        {
+            ParamtersInput.Procedure = false;
+        }
+
+        [TestMethod]
+        public void GenerateCSharp()
+        {
+            ParamtersInput.Linguagem = 1;
+            var chamada = new OrquestradorMapeamentoEntidade().Generate();
+            Assert.AreEqual(HttpStatusCode.OK, chamada.StatusCode, chamada.Message);
+            Assert.AreEqual(1, Directory.GetFiles(ParamtersInput.SelectedPath).Length, "Arquivo não foi encontrado.");
+            Directory.Delete(ParamtersInput.SelectedPath, true);
+        }
+
+        [TestMethod]
+        public void GenerateJava()
+        {
+            ParamtersInput.Linguagem = 2;
+            var chamada = new OrquestradorMapeamentoEntidade().Generate();
+            Assert.AreEqual(HttpStatusCode.OK, chamada.StatusCode, chamada.Message);
+            Assert.AreEqual(1, Directory.GetFiles(ParamtersInput.SelectedPath).Length, "Arquivo não foi encontrado.");
+            Directory.Delete(ParamtersInput.SelectedPath, true);
+        }
+    }
+}
